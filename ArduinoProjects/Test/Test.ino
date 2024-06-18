5e01e3dc94ab32ed413048e344b96f111c853e52
PLekh <lekh.patel@nasa.gov>
2024-06-18 10:55:00 -0500

    Experimenting in Test

--- ArduinoProjects/Test/Test.ino
+++ ArduinoProjects/Test/Test.ino
@@ -1,15 +1,28 @@
 
 #include "SR04.h"
+#include <LiquidCrystal.h>
+
+
+LiquidCrystal lcd(0, 1, 2, 3, 4, 5);
+
 #define TRIG_PIN 12
 #define ECHO_PIN 13
 SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
 long a;
 
+
+
 void setup() {
+
   pinMode(11, OUTPUT);
   digitalWrite(11, HIGH);
-   Serial.begin(9600);
-   delay(1000);
+
+  Serial.begin(9600);
+
+   lcd.begin(16, 2);
+  lcd.print("Hello, World!");
+
+  delay(1000);
    
 }
 
@@ -17,5 +30,9 @@ void loop() {
    a=sr04.Distance();
    Serial.print(a);
    Serial.println("cm");
+  
+  lcd.setCursor(0, 1);
+  lcd.print(millis() / 1000);
+
    delay(1000);
 }
