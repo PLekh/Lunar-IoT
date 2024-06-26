a1b4055f7ed686902f1d5a1fb9e36217a5248211
PLekh <lekh.patel@nasa.gov>
2024-06-26 09:39:45 -0500

    Initiating ports
    Initiating all of the PnP(Plug and play) ports and populating an array with what component is attached and what type of component it is.

--- ArduinoProjects/Ultrasonic_Display_PlugAndPlay/Ultrasonic_Display_PlugAndPlay.ino
+++ ArduinoProjects/Ultrasonic_Display_PlugAndPlay/Ultrasonic_Display_PlugAndPlay.ino
@@ -1,23 +1,49 @@
 #include <Arduino.h>
-
+#include <LiquidCrystal.h>
 
 #define LEN(arr) ((int) (sizeof (arr) / sizeof (arr)[0]))
 #define closeTo(num, check) (num > check -10 & num < check + 10)
+#define EmptyCode 0
+#define DisplayCode 1
+#define DistanceCode 2
+#define NumPorts 1
+#define NumPins 13
+#define licr 914
+#define sese 100
 
-int ports[1][13] = {
+byte codes[NumPorts];
+byte types[NumPorts];
+const byte ports[NumPorts][NumPins] = {
   {2,3,4,5,6,7,8,9,10,11,12,13,0}
 };
 
 void setup() {
-  for (int i; i<LEN(ports); i++){
-    Detect(ports[i][0]);
+  for (byte i; i<LEN(ports); i++){
+    Initiate(ports[i], i);
   }
-     
+
+
+
 }
 
 void loop() {
-  
+
 }
 
+void Initiate(byte Port[], int i){
 
+  unsigned int detectionVal = analogRead(Port[LEN(Port)-1]);
 
+  if(closeTo(detectionVal, licr)){
+    codes[i] = licr;
+    types[i] = DisplayCode;
+  }
+  else if(closeTo(detectionVal, sese)){
+    codes[i] = sese;
+    types[i] = DisplayCode;
+  }
+  else{
+    codes[i] = 0;
+    types[i] = EmptyCode;
+  }
+}
