36bc2b5ef4e4d20796399911f84ce46293ce2788
PLekh <lekh.patel@nasa.gov>
2024-06-26 15:11:33 -0500

    Print to external LCD
    Code traverses through the different ports and determines the best port to display on and then begins and displays on the LCD

--- ArduinoProjects/Ultrasonic_Display_PlugAndPlay/Ultrasonic_Display_PlugAndPlay.ino
+++ ArduinoProjects/Ultrasonic_Display_PlugAndPlay/Ultrasonic_Display_PlugAndPlay.ino
@@ -10,25 +10,87 @@
 #define NumPins 13
 #define licr 914
 #define sese 100
+#define sensor1 1
+#define delaytime = 1000
 
 
 byte codes[NumPorts] = { };
 byte types[NumPorts] = { };
+byte used[NumPorts] = { };
 byte priorities[NumPorts] = { };
 const byte ports[NumPorts][NumPins] = {
   {2,3,4,5,6,7,8,9,10,11,12,13,0}
 };
 
+unsigned long previousMillis = 0;
+
 void setup() {
-  for (byte i; i<LEN(ports); i++){
+  delay(1000);
+    for (byte i; i<LEN(ports); i++){
     Initiate(ports[i], i);
   }
-
-
-
+  
+  Serial.begin(9600);
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
+if (currentMillis - previousMillis >= delaytime) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    int sensorval1 = analogRead(sensor1);
+  char sensorval1str[5];
+  sprintf(sensorval1str,"%d", sensorval1);
+  //Serial.println(sensorval1str);
+  ExtDisp(sensorval1str);
+    
+  }
+
+}
+
+void ExtDisp(printstring){
+
+  /*for(byte i = 0; i <= LEN(types)-1; i++){
+    if(types[i] == 1 & codes[i] == licr){
+      
+    }
+    else if(types[i] == 1 & codes[i] == sese){
+
+    }
+
+    else{
+      Serial.println("Display not found, printstring is %s", printstring)
+    }*/
+    byte bestport = BestPort(DisplayCode)
+    if(code[bestport] == licr){
+        LiquidCrystal lcd(ports[bestport][0], ports[bestport][1], ports[bestport][2]
+        , ports[bestport][3]ports[bestport][4], ports[bestport][5]);
+
+        lcd.begin(16,2);
+        lcd.print(printstring);
+    }
+
+  }
+
+void BestPort(int typeOfComponent){
+  int highvalue = 0;
+  int value = 0
+  byte bestport = 0;
+  
+  for(byte i = 0; i <= LEN(types)-1; i++){
+    
+    if(type[i] == typeOfComponent){
+      value = priorities[i] - used[i] * 2
+      
+      if(highvalue < value){
+        highvalue = value
+        bestport = i;
+      }
+    }
+  }
+  return bestport
+}
 
 }
 
@@ -40,15 +102,18 @@ void Initiate(byte Port[], int i){
     codes[i] = licr;
     types[i] = DisplayCode;
     priorities[i] = 0;
+    Serial.println("Port %d has a Liquid Crystal", i);
   }
   else if(closeTo(detectionVal, sese)){
     codes[i] = sese;
     types[i] = DisplayCode;
     priorities[i] = 1;
+    Serial.println("Port %d has a Seven Segment", i);
   }
   else{
     codes[i] = 0;
     types[i] = EmptyCode;
     priorities[i] = 254;
+    Serial.println("Port %d is empty", i);
   }
 }
